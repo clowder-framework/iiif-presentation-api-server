cd9a9a0728cb0cc2785ed2727f65f0355c39dff8
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2025-04-11 17:01:07 -0500

    Update endpoint to return manifests with two canvases.

--- app/api/routes/manifests.py
+++ app/api/routes/manifests.py
@@ -10,15 +10,25 @@ from app.core.config import settings
 router = APIRouter(prefix="/manifests", tags=["manifests"])
 
 
-@router.get("/{image_filename}/manifest.json")
-async def read_manifests(image_filename, request: Request):
+@router.get("/{image_filename_prefix}/manifest.json")
+async def read_manifests(image_filename_prefix, request: Request):
     base_url = urljoin(str(request.base_url), settings.API_V1_STR)
     manifest = Manifest(id=f"{base_url}/manifest.json",
-                        label={"en": ["A whole-slide microscopy image."]})
+                        label={"en": ["A whole-slide microscopy image."]},
+                        behavior="individuals")
+    # TODO: See if there is a way to generalize this endpoint. Currently, this is tied to the ncsa.file.czi extractor.
     try:
-        manifest.make_canvas_from_iiif(
-            url=f"{settings.IIIF_IMAGE_API_SERVER_URL}/{image_filename}/info.json",
-            id=f"{base_url}/canvas/p1")
+        canvas_1 = manifest.make_canvas_from_iiif(
+            url=f"{settings.IIIF_IMAGE_API_SERVER_URL}/{image_filename_prefix}{settings.x_pol_postfix}/info.json",
+            id=f"{base_url}/canvas/p1",
+            label={"en": ["A whole-slide microscopy image. Cross Polarized"]},
+        )
+
+        canvas_2 = manifest.make_canvas_from_iiif(
+            url=f"{settings.IIIF_IMAGE_API_SERVER_URL}/{image_filename_prefix}{settings.p_pol_postfix}/info.json",
+            id=f"{base_url}/canvas/p2",
+            label={"en": ["A whole-slide microscopy image. P Polarized"]},
+        )
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
     return json.loads(manifest.json(indent=2))
--- app/core/config.py
+++ app/core/config.py
@@ -9,13 +9,16 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "iiif-presentation-server"
     CORS_ORIGINS: List[AnyHttpUrl] = Field(default_factory=list)
     IIIF_IMAGE_API_SERVER_URL: str = "http://localhost:8182/iiif/3"
+    x_pol_postfix: str = "_x_pol_rgb_lzw_bigtiff.tif"
+    p_pol_postfix: str = "_p_pol_rgb_lzw_bigtiff.tif"
 
     @validator("CORS_ORIGINS", pre=True, always=True)
     def set_cors_origins(cls, v):
         return v or [
             "http://localhost:3000",
             "http://localhost:3001",
-            "http://localhost:3002"
+            "http://localhost:3002",
+            "http://localhost:4444",
         ]
 
 
